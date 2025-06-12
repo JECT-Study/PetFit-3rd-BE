af52282afca00d94570641ae8c1c97db79c52418
Chaeruin <cms3604@naver.com>
2025-06-12 11:13:36 +0900

    init: initialize project

---
+++ src/main/java/ject/petfit/PetfitApplication.java
@@ -0,0 +1,13 @@
+package ject.petfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ject/petfit/PetfitApplicationTests.java
@@ -0,0 +1,13 @@
+package ject.petfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
