f97637adf74424a3dc2a3a5bd8de387889c10630
Chaeruin <cms3604@naver.com>
2025-06-16 15:47:45 +0900

    feat: 카카오 OAuth2.0인증 처리 위한 KakaoUtil 유틸 클래스 구현 및 webflux 비동기 dependency 추가

--- src/main/java/ject/petfit/domain/user/common/util/KakaoUtil.java
+++ src/main/java/ject/petfit/domain/user/common/util/KakaoUtil.java
@@ -1,4 +1,66 @@
 package ject.petfit.domain.user.common.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
 public class KakaoUtil {
+    @Value("${spring.kakao.auth.client}")
+    private String client;
+    @Value("${spring.kakao.auth.redirect}")
+    private String redirect;
+    private final WebClient webClient;
+
+    public KakaoUtil(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    public KakaoDTO.OAuthToken requestToken(String accessCode) {
+        return webClient.post()
+                .uri("https://kauth.kakao.com/oauth/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(params(accessCode)))
+                .retrieve()
+                .onStatus(status -> !status.is2xxSuccessful(),
+                        response -> Mono.error(new AuthHandler(ErrorStatus._OAUTH_SERVER_ERROR)))
+                .bodyToMono(KakaoDTO.OAuthToken.class)
+                .doOnNext(token -> log.info("oAuthToken : {}", token.getAccess_token()))
+                .block();
+    }
+
+    public KakaoDTO.KakaoProfile requestProfile(KakaoDTO.OAuthToken oAuthToken) {
+        return webClient.get()
+                .uri("https://kapi.kakao.com/v2/user/me")
+                .header("Authorization", "Bearer " + oAuthToken.getAccess_token())
+                .retrieve()
+                .onStatus(status -> !status.is2xxSuccessful(),
+                        response -> Mono.error(new AuthHandler(ErrorStatus._PROFILE_REQUEST_ERROR)))
+                .bodyToMono(KakaoDTO.KakaoProfile.class)
+                .block();
+    }
+
+    private MultiValueMap<String, String> params(String accessCode) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", client);
+        params.add("redirect_uri", redirect);
+        params.add("code", accessCode);
+        return params;
+    }
 }
