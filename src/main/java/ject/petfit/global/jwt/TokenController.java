c014f228fd790acd61d526fdd0237c69bf5c0641
Chaeruin <cms3604@naver.com>
2025-06-21 11:56:48 +0900

    refactor: jwt refresh token 재발급 컨트롤러 수정 및 쿠키로 리팩토링

--- src/main/java/ject/petfit/global/jwt/TokenController.java
+++ src/main/java/ject/petfit/global/jwt/TokenController.java
@@ -1,10 +1,12 @@
 package ject.petfit.global.jwt;
 
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.UUID;
 import ject.petfit.domain.user.entity.AuthUser;
 import ject.petfit.global.jwt.dto.RefreshTokenRequestDTO;
 import ject.petfit.global.jwt.dto.TokenResponseDTO;
 import ject.petfit.global.jwt.refreshtoken.RefreshTokenService;
+import ject.petfit.global.jwt.util.CookieUtils;
 import ject.petfit.global.jwt.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,9 +26,9 @@ public class TokenController {
     @Value("${spring.jwt.refresh-token-validity-seconds}")
     private long refreshTokenValiditySeconds;
 
-    // 리프레시 코드 재발급
+    // Refresh Token 재발급
     @PostMapping("/auth/refresh")
-    public ResponseEntity<TokenResponseDTO> refresh(@RequestBody RefreshTokenRequestDTO request) {
+    public ResponseEntity<Void> refresh(@RequestBody RefreshTokenRequestDTO request, HttpServletResponse httpServletResponse) {
         // 토큰 유효성 검사 및 삭제
         AuthUser authUser = refreshTokenService.validateAndRotateToken(request.getRefreshToken());
         // 새로운 access 토큰 발급
@@ -35,6 +37,9 @@ public class TokenController {
         String newRefreshToken = refreshTokenService.createOrUpdateRefreshToken(authUser, UUID.randomUUID().toString(),
                 refreshTokenValiditySeconds).getToken();
         //쿠키 처리
-        return ResponseEntity.ok(new TokenResponseDTO(newAccessToken, newRefreshToken));
+        httpServletResponse.addCookie(CookieUtils.addCookie("access_token", newAccessToken));
+        httpServletResponse.addCookie(CookieUtils.addCookie("refresh_token", newRefreshToken));
+
+        return ResponseEntity.ok().build();
     }
 }
