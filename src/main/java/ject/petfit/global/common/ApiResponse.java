f749b6e6364af91dba229eebb0c25d4681fbdf51
hyun <chyun5197@gmail.com>
2025-06-22 16:23:36 +0900

    fix: 공통응답 변수명 수정(data -> content)
    리액트에서 axios로 응답 받을때 response.data로 바디 내용 가져올거라 변수명 겹쳐서 헷갈릴수있음(예: response.data.data)

--- src/main/java/ject/petfit/global/common/ApiResponse.java
+++ src/main/java/ject/petfit/global/common/ApiResponse.java
@@ -9,26 +9,26 @@ public class ApiResponse<T> {
     private final boolean success;
     private final String code;
     private final String message;
-    private final T data;
+    private final T content;
 
     private final static String SUCCESS_CODE = "200";
 
     // 성공 응답 생성 메서드
-    public static <T> ApiResponse<T> success(T data) {
+    public static <T> ApiResponse<T> success(T content) {
         return ApiResponse.<T>builder()
                 .success(true)
                 .code(SUCCESS_CODE)
                 .message("Success")
-                .data(data)
+                .content(content)
                 .build();
     }
 
-    public static <T> ApiResponse<T> success(String message, T data) {
+    public static <T> ApiResponse<T> success(String message, T content) {
         return ApiResponse.<T>builder()
                 .success(true)
                 .code(SUCCESS_CODE)
                 .message(message)
-                .data(data)
+                .content(content)
                 .build();
     }
 
@@ -38,7 +38,7 @@ public class ApiResponse<T> {
                 .success(false)
                 .code(code)
                 .message(message)
-                .data(null)
+                .content(null)
                 .build();
     }
 }
