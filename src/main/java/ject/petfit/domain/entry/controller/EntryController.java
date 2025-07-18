6a3ef7abda25eba240a305d3de84459a9e834d39
chyun5197 <chyun5197@gmail.com>
2025-07-18 15:16:56 +0900

    refactor: PathVariable 요청 date 변수를 String 타입에서 LocalDate로 변경

--- src/main/java/ject/petfit/domain/entry/controller/EntryController.java
+++ src/main/java/ject/petfit/domain/entry/controller/EntryController.java
@@ -1,6 +1,7 @@
 package ject.petfit.domain.entry.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import ject.petfit.domain.entry.dto.EntryDailyResponse;
 import ject.petfit.domain.entry.dto.EntryExistsResponse;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -26,11 +28,11 @@ public class EntryController {
     @GetMapping("/{petId}/monthly/{month}")
     @Operation(summary = "월간 루틴체크, 메모, 특이사항, (일정) 유무 조회",
             description = "특정 월의 루틴 체크, 메모, 특이사항, (일정) 유무를 조회 <br> " +
-                    "{month}는 yyyy-MM 형식으로 입력 <br> " +
                     "요구사항은 아니지만 일정 유무도 응답에 포함함 ")
     public ResponseEntity<ApiResponse<List<EntryExistsResponse>>> getMonthlyEntries(
             @PathVariable Long petId,
-            @PathVariable String month
+            @Parameter(description = "yyyy-MM 형식으로 입력", example = "2025-07")
+            @PathVariable LocalDate month
     ) {
         return ResponseEntity.ok(
                 ApiResponse.success(entryService.getMonthlyEntries(petId, month))
@@ -47,7 +49,8 @@ public class EntryController {
                     "주간 조회 응답에서 일간 조회 뽑아서 사용하면 API 요청을 줄일 수 있으니 일간 조회API를 따로 만들지 않았음")
     public ResponseEntity<ApiResponse<List<EntryDailyResponse>>> getWeeklyEntries(  //
             @PathVariable Long petId,
-            @PathVariable String week
+            @Parameter(description = "yyyy-MM-dd 형식으로 입력", example = "2025-07-01")
+            @PathVariable LocalDate week  // 주간의 시작 날짜
     ) {
         return ResponseEntity.ok(
                 ApiResponse.success(entryService.getWeeklyEntries(petId, week))
