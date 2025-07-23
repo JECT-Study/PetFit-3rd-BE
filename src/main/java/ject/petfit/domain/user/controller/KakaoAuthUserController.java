8f759314043617a0d9ecf14428df6f9545e84767
Chaeruin <cms3604@naver.com>
2025-07-23 19:18:19 +0900

    feat: 프론트 도메인 임시 추가

--- src/main/java/ject/petfit/domain/user/controller/KakaoAuthUserController.java
+++ src/main/java/ject/petfit/domain/user/controller/KakaoAuthUserController.java
@@ -44,6 +44,9 @@ public class KakaoAuthUserController {
     @Value("${spring.kakao.auth.admin}")
     private String adminKey;
 
+    @Value("${spring.kakao.redirect}")
+    private String frontDomain;
+
 
     // 소셜 로그인/회원가입 -> 쿠키
     @GetMapping("/kakao/login")
@@ -59,7 +62,7 @@ public class KakaoAuthUserController {
         httpServletResponse.addCookie(CookieUtils.addCookie("access_token", accessToken));
         httpServletResponse.addCookie(CookieUtils.addCookie("refresh_token", refreshToken.getToken()));
 
-        httpServletResponse.sendRedirect("http://localhost:5173");
+        httpServletResponse.sendRedirect(frontDomain);
 
         return ResponseEntity.ok(ApiResponse.success(null));
     }
