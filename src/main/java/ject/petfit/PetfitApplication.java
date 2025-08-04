fd2fcc68686a18eca54528cbd5f7b2286acf7b3f
hyun <chyun5197@gmail.com>
2025-08-04 21:56:51 +0900

    [dev]swagger: yml에 springdoc 설정 추가

--- src/main/java/ject/petfit/PetfitApplication.java
+++ src/main/java/ject/petfit/PetfitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class PetfitApplication {
+public class PetfitApplication { //
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetfitApplication.class, args);
