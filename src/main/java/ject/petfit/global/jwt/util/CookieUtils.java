12b7d06bea3c5707b045678e7114eb2e673744f3
Chaeruin <cms3604@naver.com>
2025-08-27 14:57:38 +0900

    fix: static 원상복구

--- src/main/java/ject/petfit/global/jwt/util/CookieUtils.java
+++ src/main/java/ject/petfit/global/jwt/util/CookieUtils.java
@@ -17,39 +17,12 @@ import org.springframework.stereotype.Component;
 public class CookieUtils {
 
     @Value("${app.front.domain}")
-    private String frontDomain;
+    private static String domain;
 
-    public void addCookie(String name, String value, HttpServletResponse response) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setSecure(true);
-        cookie.setDomain(frontDomain);
-        cookie.setPath("/");
-        cookie.setMaxAge(7 * 24 * 60 * 60);
-
-        // 먼저 쿠키를 응답에 추가
-        response.addCookie(cookie);
-
-        // Set-Cookie 헤더를 읽고 SameSite=None 추가
-        Collection<String> headers = response.getHeaders("Set-Cookie");
-        boolean first = true;
-
-        for (String header : headers) {
-            if (!header.contains("SameSite")) {
-                header = header + "; SameSite=None";
-            }
-            if (first) {
-                response.setHeader("Set-Cookie", header);
-                first = false;
-            } else {
-                response.addHeader("Set-Cookie", header);
-            }
-        }
-    }
-
-    public ResponseCookie createTokenCookie(String name, String value) {
+    public static ResponseCookie createTokenCookie(String name, String value) {
         ResponseCookie cookie = ResponseCookie.from(name, value)
                 .secure(true)
-                .domain(frontDomain)
+                .domain(domain)
                 .path("/")
                 .sameSite("none")
                 .maxAge(7 * 24 * 60 * 60)
@@ -57,15 +30,14 @@ public class CookieUtils {
         return cookie;
     }
 
-    public ResponseCookie deleteTokenCookie(String name) {
+    public static ResponseCookie deleteTokenCookie(String name) {
         return ResponseCookie.from(name, "")
                 .secure(true)
-                .domain(frontDomain)
+                .domain(domain)
                 .path("/")
                 .sameSite("none")
                 .maxAge(0)  // 즉시 만료
                 .build();
     }
-
 }
 
