2e116ccdf207f7d01cb5e0143ccd8178cdfd0fe3
chyun5197 <chyun5197@gmail.com>
2025-09-19 08:22:18 +0900

    refactor: 알람 스케줄링 주기 조정 -> 10초

--- src/main/java/ject/petfit/domain/alarm/service/AlarmScheduler.java
+++ src/main/java/ject/petfit/domain/alarm/service/AlarmScheduler.java
@@ -15,12 +15,12 @@ public class AlarmScheduler { // ToDo: Batch로 옮기기
     private final AlarmRepository alarmRepository;
     private final SseEmitterService sseEmitterService;
 
-    // 3초마다 실행, 예약된 알람 전송
+    // 10초마다 실행, 예약된 알람 전송
     // 해당 시간이 지난 알람 중 아직 전송되지 않은 알람들을 여러개 보내놓고
     // 프론트에서 웹 브라우저 시간에 맞춰 알람을 띄우도록 처리
-    // ToDo: 실제 운영환경에서는 DB 부하를 고려 20초로 변경예정
+    // ToDo: 실제 운영환경에서는 DB 부하를 고려 30초로 변경예정
 
-    @Scheduled(fixedRate = 3000)
+    @Scheduled(fixedRate = 10000)
     public void sendScheduledAlarms() {
 
         // 기존 알람 처리 로직
