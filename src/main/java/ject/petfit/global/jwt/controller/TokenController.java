1f20123d7cf7bf33e3b8738d863ffb133dbd0d0d
Chaeruin <cms3604@naver.com>
2025-09-19 20:02:49 +0900

    fix: 토큰 재발급 로직 바로 쿠키 설정으로 수정

--- src/main/java/ject/petfit/global/jwt/controller/TokenController.java
+++ src/main/java/ject/petfit/global/jwt/controller/TokenController.java
@@ -5,11 +5,15 @@ import ject.petfit.domain.user.entity.AuthUser;
 import ject.petfit.domain.user.service.AuthUserService;
 import ject.petfit.global.common.ApiResponse;
 import ject.petfit.global.jwt.dto.ReissueTokenResponseDto;
+import ject.petfit.global.jwt.refreshtoken.RefreshToken;
 import ject.petfit.global.jwt.refreshtoken.service.RefreshTokenService;
+import ject.petfit.global.jwt.util.CookieUtils;
 import ject.petfit.global.jwt.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +37,7 @@ public class TokenController {
 
     // Refresh Token 재발급
     @PostMapping("/auth/refresh")
-    public ResponseEntity<ApiResponse<ReissueTokenResponseDto>> reIssue(
+    public ResponseEntity<ApiResponse<Void>> reIssue(
         @CookieValue(name = "access_token", required = false) String expiredAccessToken
     ) {
         // 쿠키가 없는 경우 처리
@@ -52,14 +56,19 @@ public class TokenController {
                     authUser.getEmail(), authUser.getMember().getRole().name(), authUser.getMember().getId());
 
             // 새로운 리프레시 토큰 생성 및 저장
-            refreshTokenService.createOrUpdateRefreshToken(authUser, UUID.randomUUID().toString(),
-                    refreshTokenValiditySeconds).getToken();
+            RefreshToken newRefreshToken = refreshTokenService.createOrUpdateRefreshToken(authUser,
+                UUID.randomUUID().toString(), refreshTokenValiditySeconds);
 
-            ReissueTokenResponseDto tokenResponseDto = new ReissueTokenResponseDto(newAccessToken);
-    
-            return ResponseEntity.status(HttpStatus.OK).body(
-                    ApiResponse.success(tokenResponseDto)
-            );
+            // SameSite=None이 적용된 쿠키 생성
+            ResponseCookie accessCookie = CookieUtils.createTokenCookie("access_token", newAccessToken);
+            ResponseCookie refreshCookie = CookieUtils.createTokenCookie("refresh_token", newRefreshToken.getToken());
+
+            return ResponseEntity.status(HttpStatus.OK)
+                .header("Authorization", "Bearer " + newAccessToken)
+                .header(HttpHeaders.SET_COOKIE, accessCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+                .body(ApiResponse.success(null));
+
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
                 ApiResponse.fail("TOKEN-401", "토큰이 유효하지 않습니다.")
